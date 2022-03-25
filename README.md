75b51a0e02ff882d9fb9f3c502ef9ef5074999a4
sergii-onyx <twilight.cympak@gmail.com>
2022-03-24 21:29:09 -0400

    add event for buttons to update the page

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # weather-dashboard
+
+  Weather on selected city
+            <div class="border border-2 border-dark p-2">
+                <h2 class="fw-bolder">Atlanta</h2>
+                <h5>Temp:</h5>
+                <h5>Wind:</h5>
+                <h5>Humidity:</h5>
+                <h5>UV Index:</h5>
+
+            </div>
+            <!-- 5-Day forecast -->
+            <div id="forecast">
+                5-Day Forecast
+                <div id="row" class="row">
+                    <ul class="col-2">
+                        <li>next 1 day forecast</li>
+                        <li>next 2 day forecast</li>
+                        <li>next 3 day forecast</li>
+                        <li>next 4 day forecast</li>
+                        <li>next 5 day forecast</li>
+                    </ul>
+
+                    <ul class="col-2">
+                        <li>next 1 day forecast</li>
+                        <li>next 2 day forecast</li>
+                        <li>next 3 day forecast</li>
+                        <li>next 4 day forecast</li>
+                        <li>next 5 day forecast</li>
+                    </ul>
+
+                    <ul class="col-2">
+                        <li>next 1 day forecast</li>
+                        <li>next 2 day forecast</li>
+                        <li>next 3 day forecast</li>
+                        <li>next 4 day forecast</li>
+                        <li>next 5 day forecast</li>
+                    </ul>
+
+                    <ul class="col-2">
+                        <li>next 1 day forecast</li>
+                        <li>next 2 day forecast</li>
+                        <li>next 3 day forecast</li>
+                        <li>next 4 day forecast</li>
+                        <li>next 5 day forecast</li>
+                    </ul>
+
+                    <ul class="col-2">
+                        <li>next 1 day forecast</li>
+                        <li>next 2 day forecast</li>
+                        <li>next 3 day forecast</li>
+                        <li>next 4 day forecast</li>
+                        <li>next 5 day forecast</li>
+                    </ul>
+
+                </div>
+            </div>
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,22 @@
 const keyApi = "dc404097273822a047bc3d11dfcf3f4b";
 var cityName;
-var temp;
-var wind;
-var humid;
 var uv;
-var weather = [];
 var cityInputEl = document.getElementById("city");
 var searchBtnEl = document.getElementById("search");
+var showWeatherDiv = document.getElementById("curentDay");
+var savedCitiesEl = document.getElementById("saved-cities");
 
-
-function getData(event) {
+//get value from input
+function getCity(event) {
     event.preventDefault();
     var city = cityInputEl.value.trim();
+    getData (city)
+}
+
+//get data from api for city name and pass to get rest info
+function getData (city) {
     var apiReqByCityName = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + keyApi;
+    cityInputEl.value = '';
     fetch(apiReqByCityName)
         .then(function (response) {
             // successful request 
@@ -20,9 +24,6 @@ function getData(event) {
                 response.json().then(function (data) {
                     //get city name
                     cityName = data.city.name.trim();
-                    temp = data.list[0].main.temp;
-                    wind = data.list[0].wind.speed;
-                    humid = data.list[0].main.humidity;
                     //get coordinates to make a call for uv index
                     var lat = data.city.coord.lat;
                     var lon = data.city.coord.lon;
@@ -37,16 +38,16 @@ function getData(event) {
         });
 }
 
+//get rest info
 function getOtherData(lat, lon) {
-    //api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
     var apiReqByLonLat = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + keyApi;
     fetch(apiReqByLonLat)
         .then(function (response) {
             // successful request 
             if (response.ok) {
                 response.json().then(function (data) {
-                    uv = data.daily[0].uvi;
-                    saveData();
+                    // uv = data.daily[0].uvi;
+                    saveData(data);
                 });
             } else {
                 alert('Error: City Not Found');
@@ -57,37 +58,164 @@ function getOtherData(lat, lon) {
         });
 }
 
-function saveData() {
+//save data to local storage
+function saveData(data) {
     //create obj with weather data
     var cityObj = {
         name: cityName,
-        temp: temp,
-        wind: wind,
-        humid: humid,
-        uv: uv
+        data: data
     }
 
-    // Get the existing data
-    weather = localStorage.getItem('weather');
+    var weather = localStorage.getItem('weather');
 
     // If no existing data, create an array
     // Otherwise, convert the localStorage string to an array
     weather = weather ? JSON.parse(weather) : [];
-    
-    // Add new data to localStorage Array
-    weather.push(cityObj);
+
+    if (weather.some(e => e.name === cityName)) {
+        console.log("found same city");
+    }
+    else {
+        weather.push(cityObj);
+    }
+    showSavedCities(weather);
     // Save back to localStorage
     localStorage.setItem("weather", JSON.stringify(weather));
-    displayData(weather);
+    displayData(cityObj);
 }
 
-function displayData(weather) {
+//create elements and append them to right side of display
+function displayData(cityObj) {
+    console.log(cityObj);
+
+    if (localStorage.length > 0) {
+        $("#curentDay").removeClass("hide");
+
+        // //get image data which has img-url, img-name and img-descr
+        var setName = cityObj.name;
+        var easyGet = cityObj.data.current;
+        var setImage = easyGet.weather[0].icon;
+        var iconUrl = "https://openweathermap.org/img/w/" + setImage + ".png";
+        console.log(setImage);
+        showWeatherDiv.innerHTML = '';
+
+        var date = " (" + moment().format('L') + ")";
+        $('#curentDay').append(
+            $('<div/>')
+                .attr("id", "mainDay")
+                .addClass("border border-2 border-dark p-2")
+                .append(
+                    $('<h2/>')
+                        .text(setName + date)
+                        .addClass("fw-bolder")
+                        .append(
+                            $('<img/>')
+                                .attr("src", iconUrl)
+                        ),
+                    $('<h5/>')
+                        .text("Temp: " + easyGet.temp + " °F"),
+                    $('<h5/>')
+                        .text("Wind: " + easyGet.wind_speed + " MPH"),
+                    $('<h5/>')
+                        .text("Humidity: " + easyGet.humidity + " %"),
+                    $('<h5/>')
+                        .text("UV Index: ")
+                        .append(
+                            $('<span/>')
+                                .text(easyGet.uvi)
+                                .attr("id", "UV")
+                        )
+                )
+        );
+
+        if (easyGet.uvi < 3) {
+            $('#UV')
+                .addClass("uv-low")
+        } else if (todayUV > 5) {
+            $('#UV')
+                .addClass("uv-high")
+        } else {
+            $('#UV')
+                .addClass("uv-mid")
+        }
+    }
+    $('#curentDay').append(
+        $('<div/>')
+            .attr("id", "forecast")
+            .text("5-Day Forecast")
+            .append(
+                $('<div/>')
+                    .attr("id", "row")
+                    .addClass("row")
+            )
+    )
+    var daily = cityObj.data.daily;
+    console.log(daily);
+    //fill up 5 day forecast
+    for (let i = 1; i < 6; i++) {
+        //date
+        var nextDay = moment().add((i), 'days').format("L");
+        //get icon code and url
+        var icon = daily[i].weather[0].icon
+        iconUrl = "https://openweathermap.org/img/w/" + icon + ".png";
+        //create and append ul, and li items
+        $('#row').append(
+            $('<ul/>')
+                .addClass("col-2")
+                .append(
+                    $('<li/>')
+                        .text(nextDay),
+                    $('<li/>')
+                        .append(
+                            $('<img/>')
+
+                                .attr("src", iconUrl)
+                        ),
+                    $('<li/>')
+                        .text("Temp: " + daily[i].temp.day + " °F"),
+                    $('<li/>')
+                        .text("Wind: " + daily[i].wind_speed + "MPH"),
+                    $('<li/>')
+                        .text("Humidity: " + daily[i].humidity + " %"),
+                )
+        )
+    }
+}
+
+//load data from local storage
+function loadData() {
+    var weather = localStorage.getItem('weather');
+    weather = weather ? JSON.parse(weather) : [];
+    showSavedCities(weather);
+}
+
+//show buttons which represent saved cities
+function showSavedCities(weather) {
     console.log(weather);
-    
-
+    savedCitiesEl.innerHTML = '';
+    if (weather) {
+        for (let i = 0; i < weather.length; i++) {
+            var savedCity = weather[i].name;
+            $('#saved-cities').append(
+                $('<div/>')
+                    .addClass("col-sm-12 mt-3")
+                    .append(
+                        $('<button/>')
+                            .addClass("btn btn-secondary text-dark")
+                            .text(savedCity)
+                    )
+            )
+        }
+    }
 }
 
+//get pressed button value
+function showWeatherSavedCities (event) {
+    event.preventDefault();
+    var city = event.target.innerHTML;
+    getData (city);
+}
 
-searchBtnEl.addEventListener("click", getData);
-
-//displayData();
+savedCitiesEl.addEventListener("click", showWeatherSavedCities)
+searchBtnEl.addEventListener("click", getCity);
+loadData();
